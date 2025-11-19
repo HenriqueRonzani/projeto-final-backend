0334d9eba73546f05f30a359cdaac5aa9490f494
Henrique <henriquedasilvaronzani@gmail.com>
2025-11-19 01:48:06 +0000

    Add JWT lib e Auth Login

---
+++ src/main/java/com/projeto/backend/Kanban/Auth/Controllers/AuthController.java
@@ -0,0 +1,23 @@
+package com.projeto.backend.Kanban.Auth.Controllers;
+
+import com.projeto.backend.Kanban.Auth.DTOs.AuthRequestDTO;
+import com.projeto.backend.Kanban.Auth.DTOs.AuthResponseDTO;
+import com.projeto.backend.Kanban.Auth.Services.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public AuthResponseDTO login(@Valid @RequestBody AuthRequestDTO dto) {
+        return authService.login(dto);
+    }
+}
